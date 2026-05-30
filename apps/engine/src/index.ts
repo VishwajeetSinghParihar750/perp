e8fff231cd743e21015575de94af1defc98ee57b
Vishwajeet Singh Parihar <svishwajeet750@gmail.com>
2026-05-30 12:51:25 +0000

    force exit on error

--- apps/engine/src/index.ts
+++ apps/engine/src/index.ts
@@ -5,3 +5,13 @@ engineServer.initialize();
 
 // thats it
 // on error that is not caught, the owner of this process should restart the process and it will work fine
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("Unhandled Rejection:", reason);
+  process.exit(1);
+});
